4d946e99231bd1d64fcea14de338a0ed1420e266
Lars Kellogg-Stedman <lars@redhat.com>
2015-08-07 10:37:33 -0400

    generate docs from included playbooks

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-DOCS = osp-6-7-upgrade.md
+DOCS = all-in-one.md \
+       service-by-service.md
 
 %.md: %.yml
 	sh makedoc.sh $^ > $@ || rm -f $@
 
 all: $(DOCS)
 
+clean:
+	rm -f $(DOCS)
--- makedoc.sh
+++ makedoc.sh
@@ -6,6 +6,13 @@
 # a markdown document that interleves the documentation
 # with the non-doc content as literal blocks.
 
+parse_includes () {
+	awk '
+	$2 == "include:" {print $3}
+	' $1
+}
+
+for src in $1 $(parse_includes $1); do
 awk '
 	/^#!/ {next}
 
@@ -37,4 +44,6 @@ awk '
 		printf "    %s\n", $0
 		last_was_doc=0
 	}
-' $1
+' $src
+done
+
